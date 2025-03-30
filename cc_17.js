cdae0183643c18f6ca595acbe4778092ec113693
mallikanicole <malmichael23@gmail.com>
2025-03-30 18:23:18 -0400

    first commit

---
+++ cc_17.js
@@ -0,0 +1,17 @@
+//Task 1
+//create customer class
+class Customer{
+    constructor(name,email){
+        this.name=name;//create property name
+        this.email=email;//create property email
+        this.purchaseHistory=[];//create property purchase history
+    }
+    //add purchase amount to history
+    addPurchase(amount){
+        this.purchaseHistory.push(amount);
+    }
+    //return total amount spent
+    getTotalSpent(){
+        return this.purchaseHistory.reduce((total,amount)=>total+amount,0);
+    }
+}
